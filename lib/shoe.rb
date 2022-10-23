d87756cef74868c8b68d76c1b1d9c84998ccbe05
lewiih001 <lewismunyambu@gmail.com>
2022-10-23 20:42:27 +0300

    hacked lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
-# Make your shoe class here!
+
+
 class Shoe
-    attr_reader :brand
     attr_accessor  :color,:size,:material,:condition
+    attr_reader :brand
     def initialize(brand)
         @brand=brand
-        
     end
     def cobble
-        @condtion=new
-        puts 'your shoe is new'
-    end
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+      end
 end
